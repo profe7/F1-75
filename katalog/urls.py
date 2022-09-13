745fe10964bc5f47b639776a960511e8e40791c6
Muhammad Fahreza Azka Arafat <beyond2lte@gmail.com>
2022-09-13 16:51:05 +0700

    Menyelesaikan tugas 2 PBP.

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,7 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import render_catalogue
+
+app_name = 'katalog'
+
+urlpatterns = [path('', render_catalogue, name='render_catalogue')]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def render_catalogue(request):
+    return render(request, 'katalog.html', context)
+
+data = CatalogItem.objects.all()
+context = {
+    'daftar_barang' : data,
+    'Nama' : 'Muhammad Fahreza Azka Arafat',
+    'NPM' : '2106752331'
+}
